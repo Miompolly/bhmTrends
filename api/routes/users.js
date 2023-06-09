bdeaf4fe1acf52f6552af63acb673d1fde2b4696
miompolly <57joel39@gmail.com>
2023-06-09 10:09:00 +0200

    feat:get all users

--- api/routes/users.js
+++ api/routes/users.js
@@ -32,8 +32,54 @@ router.put("/:id", verify, async (req, res) => {
   });
   
   // DELETE
+
+  router.delete("/:id", verify, async (req, res) => {
+    if (req.user.id === req.params.id || req.user.isAdmin) {
+      try {
+     
+    await User.findByIdAndDelete(req.params.id);
+        
+        res.status(200).json("User has been deleted ...");
+      } catch (err) {
+        return res.status(500).json(err);
+      }
+    } else {
+      return res.status(403).json("You can only dlete your Account");
+    }
+  });
+
   // GET
+
+  router.get("/find/:id", async (req, res) => {
+    try {
+     
+        const user = await User.findById(req.params.id);
+        
+        const { password, ...info } = user._doc;
+        res.status(200).json(info);
+      } catch (err) {
+        return res.status(500).json(err);
+      }
+    
+  });
   // GET ALL
+
+  router.get("/", verify, async (req, res) => {
+
+    const query=req.query.new;
+    if (req.user.isAdmin) {
+      try {
+     
+    const users = query ? await User.find().sort({_id:-1}).limit(10) : await User.find();
+        
+        res.status(200).json(users);
+      } catch (err) {
+        return res.status(500).json(err);
+      }
+    } else {
+      return res.status(403).json("You are not allowed to see all users");
+    }
+  });
   // GET USERS STATS
   
   module.exports = router;
