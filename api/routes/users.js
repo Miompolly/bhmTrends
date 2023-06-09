ca54794ddcdbd22209d907eeee067b6c7293d00b
miompolly <57joel39@gmail.com>
2023-06-09 12:30:45 +0200

    stats

--- api/routes/users.js
+++ api/routes/users.js
@@ -81,5 +81,46 @@ router.put("/:id", verify, async (req, res) => {
     }
   });
   // GET USERS STATS
+
+router.get("/stats",async (req,res)=>{
+    const today = new Date();
+    const lastYear = today.setFullYear(today.setFullYear()-1);
+
+    const monthsArray=[
+      "January",
+      "February",
+      "March",
+      "April",
+      "May",
+      "June",
+      "July",
+      "August",  
+      "September",
+      "October",
+      "November",
+      "December"
+    ];
+
+    try{
+const data = await User.aggregate([
+  {
+    $project :{
+      month:{$month:"$createdAt"}
+    }
+  },{
+    $group:{
+      _id:"$month",
+      total:{$sum:1}
+    }
+  }
+]);
+res.status(200).json(data);
+    }catch(err){
+      res.status(500).json(err);
+    }
+})
+
+
+
   
   module.exports = router;
